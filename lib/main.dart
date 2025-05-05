af2e5c323486d6175ce72990efbcacf51c8108b2
dzakimutammadien <94619488+dzakimutammadien@users.noreply.github.com>
2025-05-05 21:40:36 +0700

    Modifikasi State di todohomepage

--- lib/main.dart
+++ lib/main.dart
@@ -51,20 +51,25 @@ class TodoHomePage extends StatefulWidget {
 
 class _TodoHomePageState extends State<TodoHomePage> {
   final List<String> _todos = [];
+  final List<String> _filteredTodos = []; // Daftar tugas yang difilter
   final TextEditingController _controller = TextEditingController();
+  final TextEditingController _searchController = TextEditingController(); // Controller pencarian
 
   @override
   void initState() {
     super.initState();
     _loadTodos();
+    _searchController.addListener(_filterTodos); // Pantau input search
   }
 
   Future<void> _loadTodos() async {
     final prefs = await SharedPreferences.getInstance();
     final String? todosString = prefs.getString('todos');
     if (todosString != null) {
+      final todosList = List<String>.from(jsonDecode(todosString));
       setState(() {
-        _todos.addAll(List<String>.from(jsonDecode(todosString)));
+        _todos.addAll(todosList);
+        _filteredTodos.addAll(todosList);
       });
     }
   }
@@ -82,14 +87,39 @@ class _TodoHomePageState extends State<TodoHomePage> {
         _controller.clear();
       });
       _saveTodos();
+      _filterTodos(); // Update tampilan setelah tambah
     }
   }
 
   void _removeTodo(int index) {
+    final todoToRemove = _filteredTodos[index];
     setState(() {
-      _todos.removeAt(index);
+      _todos.remove(todoToRemove);
     });
     _saveTodos();
+    _filterTodos(); // Update tampilan setelah hapus
+  }
+
+  void _filterTodos() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      if (query.isEmpty) {
+        _filteredTodos
+          ..clear()
+          ..addAll(_todos);
+      } else {
+        _filteredTodos
+          ..clear()
+          ..addAll(_todos.where((todo) => todo.toLowerCase().contains(query)));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
